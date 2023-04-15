b9351a009b642e99ec324542538d65972aa2de20
luwanjia <luwanjia@aliyun.com>
2023-04-15 18:16:07 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # iptv-checker
 最简单的iptv检测脚本。
+
+linux直接运行，windows需要msys2或者mingw运行，依赖curl。
+
+命令：sh iptv-checker.sh <m3u文件>
+
+输出：
+output -- 目录，UTF-8格式，适用linux，一个频道一个文件
+output_gbk -- 目录，GBK格式，使用windows，一个频道一个文件
+all_online.m3u -- 文件，所有检测通过的频道，所有的频道都放在一个文件中
+
+仅支持m3u文件检测，有格式要求，例如：
+```
+#EXTINF:-1 tvg-id="CCTV1.cn" status="online",CCTV-1综合 (1080p)
+http://117.169.120.140:8080/live/cctv-1/.m3u8
+#EXTINF:-1 tvg-id="CCTV1.cn" status="timeout",CCTV-1综合 (1080p)
+http://183.207.249.15/PLTV/3/224/3221225530/index.m3u8
+#EXTINF:-1 tvg-id="CCTV1.cn" status="error",CCTV-1综合 (1080p)
+http://183.207.249.9/PLTV/3/224/3221225530/index.m3u8
+```
+频道信息以"#EXTINF"开头，第二行必须是http
+
---
+++ iptv-checker.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+function check_url()
+{
+	ck_url=$1
+	ck_name=$2
+	time_expire=$3
+	for i in {1, 2, 3}
+	do
+		res_url=`curl -o /dev/null -s -w "%{time_namelookup}\t%{time_connect}\t%{time_starttransfer}\t%{time_total}\t%{speed_download}\t%{http_code}\n" --max-time 1 "$1"`
+
+		time_namelookup=`echo $res_url | awk '{print $1}'`
+		time_connect=`echo $res_url | awk '{print $2}'`
+		time_starttransfer=`echo $res_url | awk '{print $3}'`
+		time_total=`echo $res_url | awk '{print $4}'`
+		speed_download=`echo $res_url | awk '{print $5}'`
+		http_code=`echo $res_url | awk '{print $6}'`
+		if [ `expr $time_total \< $time_expire` -eq 1 ]
+		then
+			break
+		fi
+	done
+
+	if [ ! "$http_code" == "200" ] || [ `expr $time_total \> $time_expire` -eq 1 ]
+	then 
+		echo -e "\033[31m-- [Failed ] $time_namelookup\t$time_connect\t$time_starttransfer\t$time_total\t$speed_download\t$http_code\033[0m"
+		echo -e "\033[31m--     $ck_name\033[0m"
+		echo -e "\033[31m--     $ck_url\033[0m"
+		return 1
+	else
+		echo -e "\033[33m++ [Success] $time_namelookup\t$time_connect\t$time_starttransfer\t$time_total\t$speed_download\t$http_code\033[0m"
+		echo -e "\033[33m++     $ck_name\033[0m"
+		echo -e "\033[33m++     $ck_url\033[0m"
+		return 0
+	fi
+}
+
+if [ -d "output" ]; then
+	rm output -rf
+fi
+mkdir -p output
+
+if [ -d "output_gbk" ]; then
+	rm output_gbk -rf
+fi
+mkdir -p output_gbk
+
+m3u_all="all_online.m3u"
+if [ -f "$m3u_all" ]; then
+	rm $m3u_all -f
+fi	
+
+cat $1 | while read line
+do
+	url_pre=${line%%:*}
+	if [ ! "$url_pre" = "#EXTINF" ] && [ ! "$url_pre" = "http" ]
+	then
+		continue;
+	fi
+
+	# 读取信息描述
+	if [ "$url_pre" = "#EXTINF" ]
+	then
+		m3u_info="$line"
+		continue
+	fi
+
+	# 读取URL
+	if [ "$url_pre" = "http" ]
+	then
+		m3u_url=$line
+	fi
+
+	# 获取TV名称
+	m3u_name="all"
+	if [ -n "$m3u_info" ]
+	then
+		 m3u_name=`echo ${m3u_info#*,} | sed 's/[\[\/\\\|\?\"\*\:\<\>\.]//g' | sed 's/\]//g'`
+	fi
+
+	m3u_file="output/$m3u_name.m3u"
+	m3u_file_gbk="output_gbk/$m3u_name.m3u"
+
+	# 检查URL
+	check_url "$m3u_url" "$m3u_info" 0.2
+
+	if [ $? = 0 ]
+	then
+		if [ -n "$m3u_info" ]; then
+			echo "$m3u_info" >> "$m3u_file"
+			echo "$m3u_info" >> "$m3u_all"
+		fi
+
+		echo "$m3u_url" >> "$m3u_file"
+		echo "$m3u_url" >> "$m3u_all"
+		iconv -f UTF-8 -t GBK "$m3u_file" > "$m3u_file_gbk"
+	fi
+
+   m3u_info=""
+   m3u_url=""
+done
